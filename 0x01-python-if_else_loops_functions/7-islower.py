f403fd74c56be7465b907fb81efcb278cda7068d
asaad-chadi <125455763+asaad-chadi@users.noreply.github.com>
2024-04-07 23:15:01 +0000

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    for s in range(97, 123):
-        if (ord(c) == s):
-            return True
-        else:
-            continue
-    return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for c in range(len(str)):
-        n = ord(str[c])
-        if (n >= 97 and n <= 122):
-            n -= 32
-        print("{}".format(chr(n)), end="")
-    print()
